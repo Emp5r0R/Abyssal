fcf086e37e1b8ae6969a964c4def4221321064dc
Emp5r0R <141765115+Emp5r0R@users.noreply.github.com>
2026-09-01 15:18:16 +0530

    chore: consolidate dependency maintenance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -139,7 +139,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation("net.java.dev.jna:jna:5.18.1")
-    testImplementation("org.json:json:20240303")
+    testImplementation("org.json:json:20260814")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
